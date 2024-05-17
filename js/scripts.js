01d5510aebce2bf4b0d7b53d4cd63fd10862798d
GOGulperi <gulperioztangunenc@gmail.com>
2024-05-17 15:51:38 +0200

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 console.log("Hoy! Welcome to my Portfolio Site")
+
+var age = 39;
+var name = "John";
+
+var personParent = {
+  name: name,
+  age: age,
+  child: {
+    name: "Sara"
+  }
+};
+var person = john
+function testFunction() {
+  person.age = 40;
+  //do something else
+}
+
+console.log(personParent["name"]); // The same as personParent.name
+var weWantThisField = "age";
+console.log(personParent[weWantThisField]); // The same as personParent.age
