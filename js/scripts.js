6d6ae21da4b0eb4499705cc9e64e2127c344f646
GOGulperi <gulperioztangunenc@gmail.com>
2024-05-27 15:00:34 +0200

    hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,20 @@ function testFunction() {
 console.log(personParent["name"]); // The same as personParent.name
 var weWantThisField = "age";
 console.log(personParent[weWantThisField]); // The same as personParent.age
+
+
+
+$(document).ready(function() {
+  $('.navtoggle').click(function() {
+    $('.navigation').slideToggle('fast');
+  });
+});
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
